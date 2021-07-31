c8fd39151b456a59cf1f16601858b57d94f9b40d
samvel.musheghyan <samvel.musheghyan@betconstruct.com>
2021-08-01 01:13:36 +0400

    finale change for correct work and full comfort

---
+++ app/src/main/java/com/example/homework64/Main.java
@@ -0,0 +1,8 @@
+package com.example.homework64;
+
+public class Main {
+    public static void main(String[] args) {
+        //write your code here
+        System.out.println("Hello java group 64");
+    }
+}
